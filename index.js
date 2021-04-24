af429ea21d1353dfb2a3e367e0bf6982595ae38e
Eric Lang <ealang@fb.com>
2021-04-23 23:11:56 -0400

    Fill screen

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
-function makeCamera(size) {
-  const camera = new THREE.OrthographicCamera(-size/2, size/2, size/2, -size/2, 1, 10);
+function makeCamera(width, height) {
+  const camera = new THREE.OrthographicCamera(-width/2, width/2, height/2, -height/2, 1, 10);
   camera.rotation.z = Math.PI;
   camera.position.set(0, 0, 5);
   camera.lookAt(0, 0, 0);
@@ -9,9 +9,9 @@ function makeCamera(size) {
   return camera;
 }
 
-function makePlane(size, shaderMaterial) {
+function makePlane(width, height, shaderMaterial) {
   return new THREE.Mesh(
-    new THREE.PlaneGeometry(size, size),
+    new THREE.PlaneGeometry(width, height),
     shaderMaterial,
   );
 }
@@ -25,29 +25,31 @@ const vertexShaderPromise = fetch('vertex-shader.glsl', noCache)
   .then(response => response.text());
 
 Promise.all([fragShaderPromise, vertexShaderPromise]).then(([fragmentShader, vertexShader]) => {
-  console.log(fragmentShader);
+  console.log(fragmentShader)
+  console.log(vertexShader);
 
-  const size = Math.min(window.innerWidth, window.innerHeight);
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
   const renderer = new THREE.WebGLRenderer();
 
-  renderer.setSize(size, size);
+  renderer.setSize(width, height);
   document.body.appendChild(renderer.domElement);
 
   const scene = new THREE.Scene();
 
-  const camera = makeCamera(size);
+  const camera = makeCamera(width, height);
   scene.add(camera);
 
   const shaderMaterial = new THREE.ShaderMaterial( {
       vertexShader,
       fragmentShader,
       uniforms: {
-        size: { value: size },
+        resolution: { value: [width, height] },
         timeMs: { value: 0 },
       },
   });
-  const plane = makePlane(size, shaderMaterial);
+  const plane = makePlane(width, height, shaderMaterial);
   scene.add(plane);
 
   const startTime = new Date();
