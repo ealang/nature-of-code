e08f4491b58775873f67ab7a078de6afd2112ad7
Eric Lang <ealang@fb.com>
2021-04-24 15:32:48 -0400

    Add common setup library

--- demos/00-hello-world/index.js
+++ demos/00-hello-world/index.js
@@ -1,70 +1,12 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import basicFragmentSetup from '../../lib/basic-fragment-setup.js';
 
-function makeCamera(width, height) {
-  const camera = new THREE.OrthographicCamera(-width/2, width/2, height/2, -height/2, 1, 10);
-  camera.rotation.z = Math.PI;
-  camera.position.set(0, 0, 5);
-  camera.lookAt(0, 0, 0);
-  camera.updateProjectionMatrix();
-  return camera;
-}
+const loader = new THREE.TextureLoader();
+const texture = loader.load('images/noc_print.jpeg');
 
-function makePlane(width, height, shaderMaterial) {
-  return new THREE.Mesh(
-    new THREE.PlaneGeometry(width, height),
-    shaderMaterial,
-  );
-}
-
-function loadTexture(path) {
-  const loader = new THREE.TextureLoader();
-  return loader.load(path);
-}
-
-const noCache = {
-  headers: { 'Cache-Control': 'no-cache' },
-};
-
-const fragmentShader = await fetch('demos/00-hello-world/fragment-shader.glsl', noCache).then(response => response.text());
-const vertexShader = await fetch('demos/00-hello-world/vertex-shader.glsl', noCache).then(response => response.text());
-
-console.log(vertexShader);
-console.log(fragmentShader);
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(width, height);
-document.body.appendChild(renderer.domElement);
-
-const scene = new THREE.Scene();
-
-const camera = makeCamera(width, height);
-scene.add(camera);
-
-const texture = loadTexture('images/bayer.png');
-
-const shaderMaterial = new THREE.ShaderMaterial( {
-  vertexShader,
-  fragmentShader,
-  uniforms: {
-    u_resolution: { value: [width, height] },
-    u_timeMs: { value: 0 },
+basicFragmentSetup(
+  'demos/00-hello-world/fragment-shader.glsl',
+  {
     u_texture: { value: texture },
   },
-});
-const plane = makePlane(width, height, shaderMaterial);
-scene.add(plane);
-
-const startTime = new Date();
-
-function render() {
-  shaderMaterial.uniforms.u_timeMs.value = new Date() - startTime;
-
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
-}
-
-render();
+);
--- demos/00-hello-world/vertex-shader.glsl
+++
@@ -1,3 +0,0 @@
-void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, position.z, 1.0);
-}
