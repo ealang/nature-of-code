1251010093c46e572ab9f0ace5d5b59cbab10884
elang <ealang@gmail.com>
2021-05-10 14:01:05 -0400

    simplify hello world demo

--- demos/00-hello-world/fragment-shader.glsl
+++ demos/00-hello-world/fragment-shader.glsl
@@ -1,14 +1,12 @@
 uniform vec2 u_resolution;
 uniform float u_timeSec;
-uniform sampler2D u_texture;
 
 void main() {
     vec2 uv = vec2(gl_FragCoord) / u_resolution;
-    lowp vec4 tex = texture2D(u_texture, uv);
 
     float r = cos(u_timeSec * 3.14 * 0.5) * 0.5 + 0.5;
-    float g = uv.y;
-    float b = tex.b;
+    float g = uv.x;
+    float b = uv.y;
 
     gl_FragColor = vec4(r, g, b, 1.0);
 }
--- demos/00-hello-world/index.js
+++ demos/00-hello-world/index.js
@@ -1,14 +1,3 @@
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import basicFragmentSetup from '../../lib/basic-fragment-setup.js';
 
-const loader = new THREE.TextureLoader();
-const texture = loader.load('images/noc_print.jpeg');
-
-basicFragmentSetup(
-  'demos/00-hello-world/fragment-shader.glsl',
-  {
-    uniforms: {
-      u_texture: { value: texture },
-    },
-  },
-);
+basicFragmentSetup('demos/00-hello-world/fragment-shader.glsl');
